f549f46af147c6772290d380087adf3e6319a943
mortada-killer0 <mcsplayer914@gmail.com>
2023-01-22 18:37:24 +0300

    New slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,318 +1,236 @@
-function initSlideshow() {
-  "use strict";
-
-  var slideshowList = [
-    {
-      background: "images/wallpapers/rose-petals-dark.png",
-      title: "home-brand-title",
-      detail: "home-brand-summary",
-      linkTo: "#",
-      placement: "start",
-    },
-    {
-      background:
-        document.dir === "rtl"
-          ? "images/keyarts/poster_explore.png"
-          : "images/keyarts/poster_explore_rtl.png",
-      title: "home-store-title",
-      detail: "home-store-summary",
-      linkTo: "#",
-      placement: "end",
-    },
-    {
-      background: "videos/sample.mp4",
-      title: "home-os-title",
-      detail: "home-os-summary",
-      linkTo: "#",
-      placement: "start bottom",
-    },
-  ];
+async function initSlideshow() {
+  'use strict';
 
   var progress = 0;
   var isPaused = false;
-  var slideshowContainer = document.getElementById("slideshow");
-  var slideshowPosters = slideshowContainer.querySelector(".holder");
-  var backButton = slideshowContainer.querySelector(".controls .back");
-  var forwardButton = slideshowContainer.querySelector(".controls .forward");
-  var slideshowDots = slideshowContainer.querySelector(".dots");
+  var slideshowContainer = document.getElementById('slideshow');
+  var slideshowPosters = slideshowContainer.querySelector('.holder');
+  var backButton = slideshowContainer.querySelector('.controls .back');
+  var forwardButton = slideshowContainer.querySelector('.controls .forward');
+  var slideshowDots = slideshowContainer.querySelector('.dots');
+  var playPauseButton = slideshowContainer.querySelector('.playpause-button');
 
-  if (
-    slideshowContainer ||
-    slideshowPosters ||
-    backButton ||
-    forwardButton ||
-    slideshowDots
-  ) {
-    slideshowPosters.innerHTML = "";
-    slideshowDots.innerHTML = "";
-    slideshowList.forEach((slideshow, index) => {
-      var element = document.createElement("div");
-      element.classList.add("slideshow");
-      if (index == 0) {
-        element.classList.add("current");
-      }
-      switch (slideshow.placement) {
-        case "start":
-          element.classList.add("start");
-          break;
+  var client = new XMLHttpRequest();
+  client.open('GET', '/assets/slideshow.json');
+  client.onreadystatechange = function () {
+    var json = JSON.parse(client.responseText);
+    console.log(json);
 
-        case "start top":
-        case "top start":
-          element.classList.add("start");
-          element.classList.add("top");
-          break;
+    slideshowDots.innerHTML = '';
+    slideshowPosters.innerHTML = '';
+    init(json);
+  };
+  client.send();
 
-        case "start bottom":
-        case "bottom start":
-          element.classList.add("start");
-          element.classList.add("bottom");
-          break;
+  function init(json) {
+    json.forEach((slideshow, index) => {
+      var dot = document.createElement('div');
+      dot.classList.add('dot');
+      slideshowDots.appendChild(dot);
+      dot.addEventListener('click', () => {
+        var selectedDot = slideshowDots.querySelector('.active');
+        var selectedSlideshow = slideshowPosters.querySelector('.current');
+        if (selectedSlideshow) {
+          selectedDot.classList.remove('active');
+          selectedSlideshow.classList.remove('current');
+        }
+        dot.classList.add('active');
+        element.classList.add('current');
+        updateSlideshowVideo();
+      });
 
-        case "end":
-          element.classList.add("end");
-          break;
+      var dotLabel = document.createElement('span');
+      dotLabel.classList.add('label');
+      dotLabel.dataset.l10nId = slideshow.title;
+      dot.appendChild(dotLabel);
 
-        case "end top":
-        case "top end":
-          element.classList.add("end");
-          element.classList.add("top");
-          break;
+      var dotProgress = document.createElement('div');
+      dotProgress.classList.add('progress');
+      dot.appendChild(dotProgress);
 
-        case "end bottom":
-        case "bottom end":
-          element.classList.add("end");
-          element.classList.add("bottom");
-          break;
-
-        case "top":
-          element.classList.add("top");
-          break;
-
-        case "bottom":
-          element.classList.add("bottom");
-          break;
-
-        default:
-          element.classList.add("start");
-          break;
-      }
+      var element = document.createElement('div');
+      element.classList.add('slideshow');
       slideshowPosters.appendChild(element);
 
-      if (slideshow.background.endsWith(".mp4")) {
-        var background = document.createElement("video");
-        background.src = slideshow.background;
-        background.classList.add('background');
-        element.appendChild(background);
-
-        setInterval(() => {
-          if (element.classList.contains('current') && !isPaused) {
-            if (background.isPaused) {
-              background.play();
-            }
-          } else {
-            if (!background.isPaused) {
-              background.currentTime = 0;
-              background.pause();
-            }
-          }
-        }, 300);
-      } else {
-        var background = document.createElement("img");
-        background.src = slideshow.background;
-        background.classList.add('background');
-        background.onload = () => {
-          setTimeout(() => {
-            colorPicker(slideshow.background, { colors: 2 }).then((colors) => {
-              element.style.setProperty("--color-primary", colors[0]);
-              dot.style.setProperty("--color-primary", colors[0]);
-
-              switch (lightOrDark(colors[0])) {
-                case "light":
-                  colorPicker(slideshow.background, {
-                    colors: 2,
-                    brightness: 0.25,
-                  }).then((colors) => {
-                    element.style.setProperty("--color-secondary", colors[1]);
-                    dot.style.setProperty("--color-secondary", colors[1]);
-                  });
-                  break;
-
-                case "dark":
-                  colorPicker(slideshow.background, {
-                    colors: 2,
-                    brightness: 1.75,
-                  }).then((colors) => {
-                    element.style.setProperty("--color-secondary", colors[1]);
-                    dot.style.setProperty("--color-secondary", colors[1]);
-                  });
-                  break;
-              }
-            });
-          }, 250);
-        };
-        element.appendChild(background);
+      if (index == 0) {
+        dot.classList.add('active');
+        element.classList.add('current');
       }
 
-      var context = document.createElement("div");
-      element.appendChild(context);
+      if (slideshow.background) {
+        if (slideshow.background.endsWith('.mp4')) {
+          var background = document.createElement('video');
+          background.src = slideshow.background;
+          background.classList.add('background');
+          element.appendChild(background);
+        } else {
+          var background = document.createElement('img');
+          background.src = slideshow.background;
+          background.alt = slideshow.title;
+          background.classList.add('background');
+          element.appendChild(background);
 
-      if (slideshow.wordmark) {
-        var wordmark = document.createElement("img");
-        wordmark.src = slideshow.wordmark;
-        context.appendChild(wordmark);
+          background.onload = () => {
+            setTimeout(() => {
+              colorPicker(slideshow.background, { colors: 2 }).then(
+                (colors) => {
+                  element.style.setProperty('--color-primary', colors[0]);
+                  dot.style.setProperty('--color-primary', colors[0]);
+
+                  switch (lightOrDark(colors[0])) {
+                    case 'light':
+                      colorPicker(slideshow.background, {
+                        colors: 2,
+                        brightness: 0.25,
+                      }).then((colors) => {
+                        element.style.setProperty(
+                          '--color-secondary',
+                          colors[1]
+                        );
+                        dot.style.setProperty('--color-secondary', colors[1]);
+                      });
+                      break;
+
+                    case 'dark':
+                      colorPicker(slideshow.background, {
+                        colors: 2,
+                        brightness: 1.75,
+                      }).then((colors) => {
+                        element.style.setProperty(
+                          '--color-secondary',
+                          colors[1]
+                        );
+                        dot.style.setProperty('--color-secondary', colors[1]);
+                      });
+                      break;
+                  }
+                }
+              );
+            }, 250);
+          };
+        }
       }
+
+      var content = document.createElement('div');
+      element.appendChild(content);
 
       if (slideshow.title) {
-        var title = document.createElement("h1");
+        var title = document.createElement('h1');
         title.dataset.l10nId = slideshow.title;
-        context.appendChild(title);
+        title.classList.add('title');
+        content.appendChild(title);
       }
 
       if (slideshow.detail) {
-        var detail = document.createElement("p");
+        var detail = document.createElement('p');
         detail.dataset.l10nId = slideshow.detail;
-        context.appendChild(detail);
+        detail.classList.add('detail');
+        content.appendChild(detail);
       }
 
-      if (slideshow.linkTo) {
-        var link = document.createElement("a");
-        link.href = slideshow.linkTo;
-        link.target = "_blank";
-        context.appendChild(link);
+      if (slideshow.link_to) {
+        var hyperlink = document.createElement('a');
+        hyperlink.href = slideshow.link_to;
+        hyperlink.classList.add('hyperlink');
+        content.appendChild(hyperlink);
 
-        var linkText = document.createElement("span");
-        if (slideshow.linkText) {
-          linkText.dataset.l10nId = slideshow.linkText;
+        var linkLabel = document.createElement('span');
+        hyperlink.appendChild(linkLabel);
+
+        if (slideshow.link_to.startsWith(location.origin)) {
+          linkLabel.dataset.l10nId = 'learn-more';
         } else {
-          linkText.dataset.l10nId = "learn-more";
-        }
-        link.appendChild(linkText);
-      }
-
-      var dot = document.createElement("div");
-      dot.classList.add("dot");
-      dot.addEventListener("click", () => {
-        var selectedSlideshow =
-          slideshowPosters.querySelector(".slideshow.current");
-        if (selectedSlideshow) {
-          selectedSlideshow.classList.remove("current");
-        }
-
-        var selectedDot = slideshowDots.querySelector(".dot.active");
-        if (selectedDot) {
-          selectedDot.classList.remove("active");
-        }
-
-        element.classList.add("current");
-        dot.classList.add("active");
-      });
-      if (index == 0) {
-        dot.classList.add("active");
-      }
-      slideshowDots.appendChild(dot);
-    });
-
-    setInterval(() => {
-      progress = 0;
-      if (!isPaused) {
-        var selectedSlideshow =
-          slideshowPosters.querySelector(".slideshow.current");
-        if (selectedSlideshow) {
-          selectedSlideshow.classList.remove("current");
-        }
-
-        var selectedDot = slideshowDots.querySelector(".dot.active");
-        if (selectedDot) {
-          selectedDot.classList.remove("active");
-        }
-
-        if (selectedSlideshow.nextElementSibling) {
-          selectedSlideshow.nextElementSibling.classList.add("current");
-        } else {
-          slideshowPosters.children[0].classList.add("current");
-        }
-
-        if (selectedDot.nextElementSibling) {
-          selectedDot.nextElementSibling.classList.add("active");
-        } else {
-          slideshowDots.children[0].classList.add("active");
+          linkLabel.dataset.l10nId = 'visit-site';
         }
       }
-    }, 4000);
-
-    setInterval(() => {
-      progress += 1000 / 120;
-      if (!isPaused) {
-        var selectedDot = slideshowDots.querySelector(".dot.active");
-        if (selectedDot) {
-          selectedDot.style.setProperty(
-            "--progress",
-            (progress / 4000) * 200 + "%"
-          );
-        }
-      }
-    }, 1000 / 60);
-
-    backButton.addEventListener("click", () => {
-      var selectedSlideshow =
-        slideshowPosters.querySelector(".slideshow.current");
-      if (selectedSlideshow) {
-        selectedSlideshow.classList.remove("current");
-      }
-
-      var selectedDot = slideshowDots.querySelector(".dot.active");
-      if (selectedDot) {
-        selectedDot.classList.remove("active");
-      }
-
-      if (selectedSlideshow.previousElementSibling) {
-        selectedSlideshow.previousElementSibling.classList.add("current");
-      } else {
-        slideshowPosters.children[
-          slideshowPosters.children.length - 1
-        ].classList.add("current");
-      }
-
-      if (selectedDot.previousElementSibling) {
-        selectedDot.previousElementSibling.classList.add("active");
-      } else {
-        slideshowDots.children[slideshowDots.children.length - 1].classList.add(
-          "active"
-        );
-      }
-    });
-
-    forwardButton.addEventListener("click", () => {
-      var selectedSlideshow =
-        slideshowPosters.querySelector(".slideshow.current");
-      if (selectedSlideshow) {
-        selectedSlideshow.classList.remove("current");
-      }
-
-      var selectedDot = slideshowDots.querySelector(".dot.active");
-      if (selectedDot) {
-        selectedDot.classList.remove("active");
-      }
-
-      if (selectedSlideshow.nextElementSibling) {
-        selectedSlideshow.nextElementSibling.classList.add("current");
-      } else {
-        slideshowPosters.children[0].classList.add("current");
-      }
-
-      if (selectedDot.nextElementSibling) {
-        selectedDot.nextElementSibling.classList.add("active");
-      } else {
-        slideshowDots.children[0].classList.add("active");
-      }
-    });
-
-    slideshowContainer.addEventListener("mouseenter", () => {
-      isPaused = true;
-    });
-
-    slideshowContainer.addEventListener("mouseleave", () => {
-      isPaused = false;
     });
   }
+
+  function updateSlideshowVideo() {
+    var videos = slideshowPosters.querySelectorAll('video');
+    videos.forEach((video) => {
+      video.pause();
+    });
+
+    if (!isPaused) {
+      // The video of the element we switched to
+      var selectedSlideshowVideo =
+        slideshowPosters.querySelector('.current video');
+      if (selectedSlideshowVideo) {
+        selectedSlideshowVideo.currentTime = 0;
+        selectedSlideshowVideo.play();
+      }
+      playPauseButton.dataset.icon = 'pause';
+    } else {
+      playPauseButton.dataset.icon = 'play';
+    }
+  }
+
+  function move(index) {
+    var selectedDot = slideshowDots.querySelector('.active');
+    var selectedSlideshow = slideshowPosters.querySelector('.current');
+
+    if (index == 1) {
+      if (selectedSlideshow) {
+        if (selectedSlideshow.nextElementSibling) {
+          selectedDot.classList.remove('active');
+          selectedDot.nextElementSibling.classList.add('active');
+          selectedSlideshow.classList.remove('current');
+          selectedSlideshow.nextElementSibling.classList.add('current');
+          updateSlideshowVideo();
+        } else {
+          if (slideshowPosters.firstChild) {
+            selectedDot.classList.remove('active');
+            slideshowDots.firstChild.classList.add('active');
+            selectedSlideshow.classList.remove('current');
+            slideshowPosters.firstChild.classList.add('current');
+            updateSlideshowVideo();
+          }
+        }
+      }
+    }
+
+    if (index == -1) {
+      if (selectedSlideshow) {
+        if (selectedSlideshow.previousElementSibling) {
+          selectedDot.classList.remove('active');
+          selectedDot.previousElementSibling.classList.add('active');
+          selectedSlideshow.classList.remove('current');
+          selectedSlideshow.previousElementSibling.classList.add('current');
+          updateSlideshowVideo();
+        } else {
+          if (slideshowPosters.lastChild) {
+            selectedDot.classList.remove('active');
+            slideshowDots.lastChild.classList.add('active');
+            selectedSlideshow.classList.remove('current');
+            slideshowPosters.lastChild.classList.add('current');
+            updateSlideshowVideo();
+          }
+        }
+      }
+    }
+  }
+
+  slideshowContainer.addEventListener('mouseenter', () => {
+    isPaused = true;
+    updateSlideshowVideo();
+  });
+  slideshowContainer.addEventListener('mouseleave', () => {
+    isPaused = false;
+    updateSlideshowVideo();
+  });
+  playPauseButton.addEventListener('click', () => {
+    isPaused = !isPaused;
+    updateSlideshowVideo();
+  });
+  backButton.addEventListener('click', () => {
+    move(-1);
+  });
+  forwardButton.addEventListener('click', () => {
+    move(1);
+  });
+
+  setInterval(() => {
+    if (!isPaused) move(1);
+  }, 4000);
 }
